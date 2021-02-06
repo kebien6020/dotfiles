ca0d0c69bfc3e677b6e206dc52e52f5998fe4713
Kevin Peña <kevin.pena.prog@gmail.com>
2021-02-06 01:50:26 -0500

    Tmux clipboard support and clip bash function

---
+++ system/os.sh
@@ -0,0 +1,14 @@
+# OS clipboard integration
+function clip() {
+  case "$OSTYPE" in
+    linux*)
+      if grep -q Microsoft /proc/version; then
+        /mnt/c/Windows/System32/clip.exe $@
+      else
+        xsel -i --clipboard $@
+      fi
+      ;;
+    cygwin*) clip.exe $@ ;;
+  esac
+}
+
