261080b96e263b47da4ea82414e0279f7c40ea5e
Kevin Peña <kevin.pena.prog@gmail.com>
2022-03-15 23:07:07 -0500

    Try harder to get added to the docker group

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -5,12 +5,13 @@
 
 # docker permissions
 DOCKER_SOCKET=/var/run/docker.sock
-DOCKER_GROUP=docker
+DOCKER_GROUP=docker_host
 USER=kevin
 if [ -S ${DOCKER_SOCKET} ]; then
 	DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
 	sudo addgroup --gid ${DOCKER_GID} ${DOCKER_GROUP}
 	sudo usermod --append --groups ${DOCKER_GROUP} ${USER}
+	sudo usermod --append --groups ${DOCKER_GID} ${USER}
 fi
 
 # volume permissions
