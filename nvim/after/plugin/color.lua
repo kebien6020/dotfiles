d043126f519f7a7c5db823c95ba4d6b1d6ff37d0
Kevin Peña <kpenaseveriche@twilio.com>
2023-03-02 20:44:32 -0500

    [nvim] Add OnedarkStyle and OnedarkTransparent commands

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -9,3 +9,23 @@ onedark.setup {
 }
 
 onedark.load()
+
+vim.api.nvim_create_user_command('OnedarkStyle', function(opts)
+	onedark.set_options('style', opts.fargs[1])
+	onedark.load()
+end, {
+	nargs = 1,
+	complete = function()
+		return onedark.styles_list
+	end
+})
+
+vim.api.nvim_create_user_command('OnedarkTransparent', function(opts)
+	onedark.set_options('transparent', opts.fargs[1] == 'true' and true or false)
+	onedark.load()
+end, {
+	nargs = 1,
+	complete = function()
+		return { 'true', 'false' }
+	end
+})
