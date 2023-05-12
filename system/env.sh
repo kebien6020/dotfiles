cba48e0c884a054f68ee8461d243ce74a7695313
Kevin Peña <kevin.pena.prog@gmail.com>
2023-05-11 22:41:55 -0500

    [env] Add env vars to move hidden files to .config/

--- system/env.sh
+++ system/env.sh
@@ -5,5 +5,11 @@ export EDITOR="nvim"
 export DOT=$HOME/.dotfiles
 export GPG_TTY; GPG_TTY="$(tty)"
 
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export LESSHISTFILE="${XDG_CONFIG_HOME}/lesshst"
+export NODE_REPL_HISTORY="${XDG_CONFIG_HOME}/node_repl_history"
+export TS_NODE_HISTORY="${XDG_CONFIG_HOME}/ts_node_repl_history"
+export SQLITE_HISTORY="${XDG_CONFIG_HOME}/sqlite_history"
+
 # Add home folder binaries (at the start)
 export PATH=$HOME/bin:$PATH
