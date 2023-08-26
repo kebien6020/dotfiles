227d7776ef21616e38cb66e0f965e5c169718bef
Kevin Peña <kevin.pena.prog@gmail.com>
2023-08-25 21:59:32 -0500

    [nvim] Statusline update

--- nvim/lua/me/statusline.lua
+++ nvim/lua/me/statusline.lua
@@ -1,7 +1,7 @@
 function StatusLine()
 	local file = '%f'
 	local file_status = '%h%w%m%r'
-	local lsp_status = '%-{v:lua.require\'lsp-status\'.status()}'
+	local lsp_status = '%-{%v:lua.require\'lsp-status\'.status()%}'
 	local sep = '%='
 	local loc = '%-14.(%l,%c%V%)'
 	local pct = '%P'
