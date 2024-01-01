1b58bd6e6f8691886d097d814c8e6c4d1e1091c6
Kevin Peña <kevin.pena.prog@gmail.com>
2024-01-01 14:53:17 -0500

    [terminal] Migrate alacritty config to toml

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -7,4 +7,4 @@ rm $HOME/.gitconfig; ln -s $DOT/git/.gitconfig $HOME/.gitconfig
 rm $HOME/.vimrc; ln -s $DOT/vim/.vimrc $HOME/.vimrc
 rm $HOME/.tmux.conf; ln -s $DOT/tmux/.tmux.conf $HOME/.tmux.conf
 rm $HOME/.config/nvim; ln -s $DOT/nvim/ $HOME/.config/nvim
-rm $HOME/.config/alacritty/alacritty.yml; mkdir -p $HOME/.config/alacritty; ln -s $DOT/terminal/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+rm $HOME/.config/alacritty/alacritty.{yml,toml}; mkdir -p $HOME/.config/alacritty; ln -s $DOT/terminal/alacritty.toml $HOME/.config/alacritty/alacritty.toml
