04018524e49bd7572945b3a3bd15edf2d28e4293
Kevin Peña <kevin.pena.prog@gmail.com>
2024-05-01 18:26:13 -0500

    [bash] Add kev-package-review functions

--- system/functions.sh
+++ system/functions.sh
@@ -2,3 +2,23 @@
 function mk() {
   mkdir -p "$@" && cd "$@" || return
 }
+
+function kev-package-review() {
+	paru -Qq |
+		fzf \
+			--preview='paru -Qi {}; pactree -r {}' \
+			--bind='ctrl-r:execute(paru -Rsc {})+reload(paru -Qq)' \
+			--bind='ctrl-d:execute(paru -D --asexplicit {})+reload(paru -Qq)' \
+			--bind='ctrl-f:preview(paru -Ql {})' \
+			--bind='esc:change-query()'
+}
+
+function kev-package-review-orphans() {
+	paru -Qqdt |
+		fzf \
+			--preview='paru -Qi {}; pactree -r {}' \
+			--bind='ctrl-r:execute(paru -Rsc {})+reload(paru -Qqdt)' \
+			--bind='ctrl-d:execute(paru -D --asexplicit {})+reload(paru -Qqdt)' \
+			--bind='ctrl-f:preview(paru -Ql {})' \
+			--bind='esc:change-query()'
+}
