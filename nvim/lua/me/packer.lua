d3e0a2d1ac67083873081fef9f7b7fe57bb49ec2
Kevin Peña <kevin.pena.prog@gmail.com>
2024-05-11 22:46:43 -0500

    [nvim] Remove nightly branch from the nvim-tree package

--- nvim/lua/me/packer.lua
+++ nvim/lua/me/packer.lua
@@ -22,7 +22,6 @@ return require('packer').startup(function(use)
 		requires = {
 			'nvim-tree/nvim-web-devicons', -- optional, for file icons
 		},
-		tag = 'nightly' -- optional, updated every week. (see issue #1193)
 	}
 
 	use 'mfussenegger/nvim-jdtls' -- Java additional support
