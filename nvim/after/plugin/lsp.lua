a2aa1b22726be1be5cf5d087901422f53eb04c47
Kevin Peña <kpenaseveriche@twilio.com>
2025-05-30 15:58:48 -0500

    [nvim] Remove unmaintained symbols-outline plugin

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,7 +1,6 @@
 local lspconfig = require('lspconfig')
 local lsp_attach = require 'me.utils'.lsp_attach
 local lsp_capabilities = require 'me.utils'.lsp_capabilities
-local outline = require 'symbols-outline'
 local status = require 'lsp-status'
 
 lspconfig.clangd.setup { capabilities = lsp_capabilities, on_attach = lsp_attach }
@@ -11,10 +10,6 @@ lspconfig.neocmake.setup { capabilities = lsp_capabilities, on_attach = lsp_atta
 lspconfig.hls.setup { capabilities = lsp_capabilities, on_attach = lsp_attach } -- Haskell
 
 -- ...the rest are on dedicated files
-
--- Outline
-outline.setup()
-vim.keymap.set('n', '<leader>so', vim.cmd.SymbolsOutline)
 
 -- Status Line at the bottom
 status.config { indicator_ok = '[OK]', status_symbol = '' }
