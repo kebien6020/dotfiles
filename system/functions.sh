f8f62e02341d84fa7bce2505a45156dc05a4bb74
Kevin Peña <kevin.pena.prog@gmail.com>
2026-03-09 10:26:15 -0500

    [bash] Update arch-linux paru helpers

--- system/functions.sh
+++ system/functions.sh
@@ -3,22 +3,32 @@ function mk() {
   mkdir -p "$@" && cd "$@" || return
 }
 
-function kev-package-review() {
-	paru -Qq |
+function fzf-paru() {
+	cmd="$1"
+	$cmd |
 		fzf \
 			--preview='paru -Qi {}; pactree -r {}' \
-			--bind='ctrl-r:execute(paru -Rsc {})+reload(paru -Qq)' \
-			--bind='ctrl-d:execute(paru -D --asexplicit {})+reload(paru -Qq)' \
+			--bind="ctrl-r:execute(paru -Rsc {})+reload($cmd)" \
+			--bind="ctrl-d:execute(paru -D --asexplicit {})+reload($cmd)" \
 			--bind='ctrl-f:preview(paru -Ql {})' \
+			--bind='ctrl-t:preview(pactree {})' \
 			--bind='esc:change-query()'
 }
 
+function kev-package-review() {
+	fzf-paru 'paru -Qq'
+}
+
 function kev-package-review-orphans() {
-	paru -Qqdt |
-		fzf \
-			--preview='paru -Qi {}; pactree -r {}' \
-			--bind='ctrl-r:execute(paru -Rsc {})+reload(paru -Qqdt)' \
-			--bind='ctrl-d:execute(paru -D --asexplicit {})+reload(paru -Qqdt)' \
-			--bind='ctrl-f:preview(paru -Ql {})' \
-			--bind='esc:change-query()'
+	fzf-paru 'paru -Qqdt'
+}
+
+function kev-package-review-manual() {
+	script=$(mktemp)
+	trap 'rm -f "$script"' RETURN
+
+	echo '#!/bin/bash' > "$script"
+	echo 'comm -23 <(paru -Qqett | sort) <(paru -Qqg base-devel | sort | uniq)' >> "$script"
+	chmod +x "$script"
+	fzf-paru "$script"
 }
