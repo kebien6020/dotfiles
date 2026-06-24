39c3d4f7a5d608f61b55a06f55723f5987f24ce5
Kevin Peña <kpenaseveriche@twilio.com>
2026-06-24 15:51:29 -0500

    [nvim] Add auto-formatting back for tf, add :Format

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -28,3 +28,20 @@ vim.api.nvim_create_user_command('LspLog', function()
 	local path = vim.lsp.log.get_filename()
 	vim.cmd('edit ' .. path)
 end, { desc = 'Open LSP log file' })
+
+-- Format command
+vim.api.nvim_create_user_command('Format', function()
+	vim.lsp.buf.format { async = false }
+end, { desc = 'Format current buffer with LSP' })
+
+-- Auto-format for specific filetypes
+vim.api.nvim_create_autocmd('BufWritePre', {
+	callback = function()
+		local matches = vim.bo.filetype == 'terraform'
+		if not matches then
+			return
+		end
+
+		vim.lsp.buf.format { async = false }
+	end
+})
